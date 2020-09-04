4c0bd422da740f0d4749fc3647985c9c535a7873
Jessica Murray <Jessicavmurray47@gmail.com>
2020-09-04 11:26:09 -0700

    First R changes

---
+++ hello.R
@@ -0,0 +1,9 @@
+n=100000 
+die1 = sample(1:6,n,replace=TRUE) 
+die2 = sample(1:6,n,replace=TRUE) 
+die3 = sample(1:6,n,replace=TRUE) 
+diesum = die1+die2+die3 
+prob = 0 
+for (i in 1:n) if (diesum[i] < 5) prob = prob+1 
+prob = prob/n 
+print(prob) 
